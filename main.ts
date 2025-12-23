9c95301a67881b75122637066875ff618b361f83
Jmeow <j@jmeow.net>
2025-12-23 10:29:56 -0600

    Get emails by id, subject, to address

--- main.ts
+++ main.ts
@@ -129,11 +129,27 @@ Deno.serve({ port: 8045 }, async (req) => {
     const url = new URL(req.url)
 
     if (url.pathname.startsWith("/email/")) {
-        const emailId = url.pathname.slice(7).split("/")[0]
-        const emailResult =
-            await sql`SELECT "from", "to", "body", "headers", "subject" FROM public.emails WHERE id=${emailId}`
+        const type = url.pathname.slice(7).split("/")[0]
+        const info = url.pathname.slice(7).split("/")[1]
+        let emailResult
+        switch (type) {
+            case "id":
+                emailResult =
+                    await sql`SELECT "id", "from", "to", "body", "headers", "subject" FROM public.emails WHERE id=${info}`
+                break
+            case "subject":
+                emailResult =
+                    await sql`SELECT "id", "from", "to", "body", "headers", "subject" FROM public.emails WHERE subject=${info}`
+                break
+            case "to":
+                emailResult =
+                    await sql`SELECT "id", "from", "to", "body", "headers", "subject" FROM public.emails WHERE "to"=${info}`
+                break
+            default:
+                return new Response(null, { status: 404 })
+        }
         if (emailResult.length) {
-            const returnVal = emailResult[0]
+            const returnVal = emailResult.sort((a, b) => b.id - a.id)[0]
             returnVal.headers = JSON.parse(returnVal.headers)
             return new Response(JSON.stringify(returnVal), {
                 headers: { "Content-Type": "application/json" },
