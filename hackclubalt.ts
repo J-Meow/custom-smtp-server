e3b5f6830a44a5d9fb260454bf47c47881631916
Jmeow <j@jmeow.net>
2025-12-28 11:47:36 -0600

    Signing up

--- hackclubalt.ts
+++ hackclubalt.ts
@@ -1,4 +1,5 @@
-const email = "testalthackclub@customsmtp.jmeow.net"
+const altID = 1
+const email = "jmeow.hc.alt." + altID + "@customsmtp.jmeow.net"
 
 import { launch } from "jsr:@astral/astral"
 
@@ -10,6 +11,14 @@ await using page = await browser.newPage("https://account.hackclub.com")
 await page.waitForNetworkIdle()
 await (await page.$(`input[type="email"]`))!.type(email)
 await (await page.$(`button[type="submit"]`))!.click()
+await page.waitForNavigation()
+if (!(await page.$(`#code-input-code`))) {
+    await (await page.$(`#identity_first_name`))!.type("JmeowAlt")
+    await (await page.$(`#identity_last_name`))!.type("Id " + altID)
+    await (await page.$(`#identity_birthday`))!.type("01012010")
+    await (await page.$(`input[type="submit"]`))!.click()
+    await page.waitForNavigation()
+}
 
 async function getOTP() {
     await fetch(
@@ -55,15 +64,68 @@ async function getOTP() {
     return otp
 }
 
-await page.waitForNavigation()
 await (await page.$(`#code-input-code`))!.type(await getOTP())
 await (await page.$(`input[type="submit"]`))!.click()
-await page.waitForNavigation()
-await page.goto("https://hackclub.slack.com", { waitUntil: "load" })
-await (await page.$(".c-button--primary"))!.click()
+try {
+    await page.waitForNavigation()
+} catch (_) {
+    console.log("Navigation took too long")
+}
+try {
+    await (await page.$(".c-button--primary"))!.click()
+} catch (_) {
+    console.log("No putton to press")
+}
+try {
+    await page.goto("https://hackclub.slack.com", { waitUntil: "load" })
+} catch (_) {
+    console.log("Network Idle took too long")
+}
+try {
+    await (await page.$(".c-button--primary"))!.click()
+} catch (_) {
+    console.log("No putton to press")
+}
+try {
+    await page.waitForNavigation({ waitUntil: "load" })
+    await page.waitForTimeout(1000)
+} catch (_) {
+    console.log("Loading took too long")
+}
+try {
+    if (page.url.includes("/tos")) {
+        await (await page.$(".c-button--primary"))!.click()
+    }
+} catch (_) {
+    console.log("No TOS button")
+}
+await page.waitForFunction(`!!document.querySelector("div.p-client_container")`)
+await page.waitForTimeout(1000)
+if (await page.$(`[data-qa="setup_page_welcome_next"]`)) {
+    await (await page.$(`[data-qa="setup_page_welcome_next"]`))!.click()
+}
+await page.waitForTimeout(1000)
+if (await page.$(`[data-qa="setup_page_people_skip"]`)) {
+    await (await page.$(`[data-qa="setup_page_people_skip"]`))!.click()
+}
+
 await page.waitForFunction(
     `!!document.querySelector("div.p-top_nav__search__container")`,
 )
+console.log("Slack loaded")
+try {
+    await page.waitForTimeout(3000)
+    if (await page.$(`[data-qa-action-id="coc_continue"]`)) {
+        await (await page.$(`[data-qa-action-id="coc_continue"]`))!.click()
+        await page.waitForFunction(
+            `!!document.querySelector('[data-qa-action-id="tutorial_agree"]')`,
+        )
+        await (await page.$(`[data-qa-action-id="tutorial_agree"]`))!.click()
+        await page.waitForTimeout(3000)
+    }
+} catch (_) {
+    console.log("No code of conduct to accept")
+}
 const xoxd = (await page.cookies("https://app.slack.com")).filter(
     (x) => x.name == "d",
 )[0].value
