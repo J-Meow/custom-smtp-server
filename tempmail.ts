86cfcbbdd8622566147ca2b2b914551fbd366a32
Jmeow <j@jmeow.net>
2026-01-04 20:05:29 -0600

    bad looking tempmail UI with emails logged to the console and IP-based email assignment

---
+++ tempmail.ts
@@ -0,0 +1,64 @@
+const emailEnd = "@customsmtp.jmeow.net"
+const activeEmails: { [key: string]: { email: string } } = {}
+
+Deno.serve({ port: 6853 }, async (req, connInfo) => {
+    console.log(connInfo.remoteAddr.hostname)
+    const url = new URL(req.url)
+    if (url.pathname == "/" && req.method == "GET") {
+        return new Response(Deno.readTextFileSync("tempmail.html"), {
+            status: 200,
+            headers: {
+                "Content-Type": "text/html",
+            },
+        })
+    }
+    function newEmail(host: string) {
+        let email = ""
+        email += "tempemail"
+        email += Math.floor(Math.random() * 10000000)
+            .toString()
+            .padStart(7, "0")
+        email += emailEnd
+        activeEmails[host] = {
+            email,
+        }
+    }
+    if (url.pathname == "/getEmail" && req.method == "GET") {
+        if (!(connInfo.remoteAddr.hostname in activeEmails)) {
+            newEmail(connInfo.remoteAddr.hostname)
+        }
+        return new Response(activeEmails[connInfo.remoteAddr.hostname].email, {
+            status: 200,
+        })
+    }
+    if (url.pathname == "/emails" && req.method == "GET") {
+        if (!(connInfo.remoteAddr.hostname in activeEmails)) {
+            newEmail(connInfo.remoteAddr.hostname)
+        }
+        const email = activeEmails[connInfo.remoteAddr.hostname].email
+        const response = await (
+            await fetch(
+                "http://" +
+                    Deno.env.get("SERVERURL") +
+                    ":8045/emails/to/" +
+                    email,
+                {
+                    headers: {
+                        authorization: "Bearer " + Deno.env.get("APIKEY"),
+                    },
+                },
+            )
+        ).text()
+        return new Response(response.length ? response : "[]", {
+            headers: { "Content-Type": "application/json" },
+            status: 200,
+        })
+    }
+    if (url.pathname == "/newEmail" && req.method == "POST") {
+        newEmail(connInfo.remoteAddr.hostname)
+        return new Response(null, {
+            status: 204,
+        })
+    }
+    return new Response(null, { status: 404 })
+})
